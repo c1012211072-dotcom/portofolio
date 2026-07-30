422988e9ba9af5295541f8f790d05b9c982088d8
c1012211072-dotcom <c1012211072@student.untan.ac.id>
2026-07-30 12:37:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  },
+
+  {
+    threshold: 0.25,
+  }
+);
+
+hiddenElements.forEach((el) => observer.observe(el));
